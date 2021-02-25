b510a4919c63f6ed07aeb82e76d5b7e9a8c399fd
ysonbrian <znlsakxls@gmail.com>
2021-02-25 16:21:51 +0900

    Check the Register Authentication

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -25,15 +25,34 @@ const AuthState = (props) => {
 	const [state, dispatch] = useReducer(authReducer, initialState);
 
 	// Load User
-
+	const loadUser = () => console.log('loadUser');
 	// Register User
+	const register = async (formData) => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		try {
+			const res = await axios.post('/api/users', formData, config);
 
+			dispatch({
+				type: REGISTER_SUCCESS,
+				payload: res.data,
+			});
+		} catch (err) {
+			dispatch({
+				type: REGISTER_FAIL,
+				payload: err.response.data.msg,
+			});
+		}
+	};
 	// Login User
-
+	const login = () => console.log('login');
 	// Logout
-
+	const logout = () => console.log('logout');
 	// Clear Errors
-
+	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 	return (
 		<AuthContext.Provider
 			value={{
@@ -42,6 +61,11 @@ const AuthState = (props) => {
 				loading: state.loading,
 				user: state.user,
 				error: state.error,
+				loadUser,
+				register,
+				login,
+				logout,
+				clearErrors,
 			}}>
 			{props.children}
 		</AuthContext.Provider>
