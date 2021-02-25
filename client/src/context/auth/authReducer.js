7b28fb066097d93e25a13ad19ec83bb9762d61f8
ysonbrian <znlsakxls@gmail.com>
2021-02-25 17:15:00 +0900

    Login & Logout functions and PrivateRoute added

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -12,7 +12,15 @@ import {
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
 	switch (action.type) {
+		case USER_LOADED:
+			return {
+				...state,
+				isAuthenticated: true,
+				loading: false,
+				user: action.payload,
+			};
 		case REGISTER_SUCCESS:
+		case LOGIN_SUCCESS:
 			localStorage.setItem('token', action.payload.token);
 			return {
 				...state,
@@ -21,6 +29,9 @@ export default (state, action) => {
 				loading: false,
 			};
 		case REGISTER_FAIL:
+		case AUTH_ERROR:
+		case LOGIN_FAIL:
+		case LOGOUT:
 			localStorage.removeItem('token');
 			return {
 				...state,
