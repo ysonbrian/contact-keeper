a2d6a3bc35afaf6f90d94a2661ee0848772fcfff
ysonbrian <znlsakxls@gmail.com>
2021-02-26 15:13:32 +0900

    Finished with CRUD && Ready to Deploy

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -50,8 +50,13 @@ const ContactState = (props) => {
 		}
 	};
 	// Delete Contact
-	const deleteContact = (id) => {
-		dispatch({ type: DELETE_CONTACT, payload: id });
+	const deleteContact = async (id) => {
+		try {
+			await axios.delete(`/api/contacts/${id}`);
+			dispatch({ type: DELETE_CONTACT, payload: id });
+		} catch (err) {
+			dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+		}
 	};
 
 	// Clear Contacts
@@ -68,8 +73,22 @@ const ContactState = (props) => {
 		dispatch({ type: CLEAR_CURRENT });
 	};
 	// Update Contact
-	const updateContact = (contact) => {
-		dispatch({ type: UPDATE_CONTACT, payload: contact });
+	const updateContact = async (contact) => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		try {
+			const res = await axios.put(
+				`/api/contacts/${contact._id}`,
+				contact,
+				config
+			);
+			dispatch({ type: UPDATE_CONTACT, payload: res.data });
+		} catch (err) {
+			dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+		}
 	};
 	// Filter Contacts
 	const filterContacts = (text) => {
